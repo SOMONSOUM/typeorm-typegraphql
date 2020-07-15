727c65e7e894b034b63b76ce9f9f83d7a2a0c300
SOMON Soum <somon.soum9@gmail.com>
2020-07-16 01:10:13 +0700

    modified:   .gitignore 	modified:   .prettierrc 	modified:   package.json 	modified:   server.ts 	new file:   src/App.ts 	new file:   tsconfig.json

--- server.ts
+++ server.ts
@@ -1,3 +1,34 @@
-const mgs = 'Hello, World!';
+import 'reflect-metadata';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema, Resolver, Query } from 'type-graphql';
+import { config } from 'dotenv';
+config();
 
-console.log(mgs);
+const app: express.Application = express();
+const path = '/graphql';
+const PORT = process.env.PORT || 8080;
+@Resolver()
+class HelloResolver {
+  @Query(() => String)
+  async hello() {
+    return 'Hello, World!';
+  }
+}
+
+const main = async () => {
+  const schema = await buildSchema({
+    resolvers: [HelloResolver],
+    validate: false,
+  });
+
+  const apolloServer = new ApolloServer({ schema });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(PORT, () => {
+    console.log(`🚀 Started http://localhost:${PORT}${path}`);
+  });
+};
+
+main();
